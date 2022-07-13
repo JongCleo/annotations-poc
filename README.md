aaadecd5dcd3ebf4e3f25999d9b6e988514c3776
Leo Kim <leonardkim96@gmail.com>
2022-07-13 01:58:32 -0400

    connected to server

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 A proof of concept for an annotations feature on Google Meets
+
+[Boilerplate Reference](https://github.com/EmailThis/extension-boilerplate/tree/master/src/scripts)
--- index.js
+++ index.js
@@ -1,30 +1,35 @@
-let express = require("express");
-let app = express()
-let httpServer =require('http').createServer(app)
-let io = require('socket.io')(httpServer)
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
-let connections = []
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "https://meet.google.com",
+  },
+  // transports: ["websocket"],
+});
 
-io.on('connect', (socket) => {
+let connections = [];
+
+io.on("connect", (socket) => {
   connections.push(socket);
   console.log(`${socket.id} has connected`);
-  
-  socket.on('sendDraw', (data) => {
-    socket.broadcast.emit('receiveDraw', {x:data.x, y:data.y})    
-  })
 
-  socket.on('sendCursor', (data) => {
-    socket.broadcast.emit('receiveCursor', {x:data.x, y:data.y})    
-  })
+  socket.on("sendDraw", (data) => {
+    socket.broadcast.emit("receiveDraw", { x: data.x, y: data.y });
+  });
 
-  socket.on('disconnect', (reason) => {
+  socket.on("sendCursor", (data) => {
+    socket.broadcast.emit("receiveCursor", { x: data.x, y: data.y });
+  });
+
+  socket.on("disconnect", (reason) => {
     console.log(`${socket.id} has disconnected`);
-    connections = connections.filter((con => con.id !== socket.id))
-  })
+    connections = connections.filter((con) => con.id !== socket.id);
+  });
+});
 
-})
-
-
-app.use(express.static('public'))
-let PORT = process.env.PORT || 8080
-httpServer.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+let PORT = process.env.PORT || 8080;
+httpServer.listen(PORT, () => console.log(`Server started on port ${PORT}`));
